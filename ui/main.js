e4db71b0f349fcdb642f3a514b1af27d0a4e61ee
ashish5477 <36543501+ashish5477@users.noreply.github.com>
2018-04-16 23:02:55 +0530

    [imad-console] Updates ui/main.js

---
+++ ui/main.js
@@ -0,0 +1,22 @@
+body {
+    font-family: sans-serif;
+    background-color: white;
+    margin-top: 75px;
+}
+
+.center {
+    text-align: center;
+}
+
+.text-big {
+    font-size: 500%;
+}
+
+.bold {
+    font-weight: bold;
+}
+
+.img-medium {
+    height: 200px;
+}
+
